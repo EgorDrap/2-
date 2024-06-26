52636151cbcb4f4e065272205b7f3048babce525
EgorDrap <drapegoroleg@mail.ru>
2024-06-26 21:36:48 +0500

    Update module1hard.py

--- module1hard.py
+++ module1hard.py
@@ -13,3 +13,15 @@ cO=(sum(c)/len(c))
 dO=(sum(d)/len(d))
 eO=(sum(e)/len(e))
 print({'Johnny':(eO), 'Bilbo':(dO), 'Steve':(cO), 'Khendrik':(bO), 'Aaron':(aO)})
+
+
+#второй вариант
+grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]]
+students = {'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}
+
+students = sorted(list(students))
+print(students)
+grades_ = [sum(grades[0])/len(grades[0]), sum(grades[1])/len(grades[1]), sum(grades[2])/len(grades[2]), sum(grades[3])/len(grades[3]), sum(grades[4])/len(grades[4])]
+print(grades_)
+A = dict(zip(students, grades_))
+print(A)
